f7e1257ee588d81d648b1cb9be64551270f04d7c
Michael Grossberg <mgrossberg@ccny.cuny.edu>
2022-11-08 13:13:28 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is for education, workshops and modules teaching python sci-stack for geochemistry',
+    author='CCNY-Earthchem',
+    license='MIT',
+)
